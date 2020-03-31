f1b6781f994f748ac49bd72dacd198eb6132e056
Andres Pino Vallejo <apino@lean-tech.io>
2020-03-31 08:38:11 -0500

    EL-1680 Add basic layout for the Need Help modal

--- src/components/NeedHelpModal/index.jsx
+++ src/components/NeedHelpModal/index.jsx
@@ -3,6 +3,9 @@ import { View, StyleSheet } from 'react-native'
 import { Modalize } from 'react-native-modalize'
 import PropTypes from 'prop-types'
 
+import Txt from '../Txt'
+import Icon from '../Icons'
+
 export default function NeedHelpModal (props) {
   const { open } = props
 
@@ -20,13 +23,52 @@ export default function NeedHelpModal (props) {
       adjustToContentHeight
       ref={modalRef}
     >
-      ...your content
+      <View>
+        <Txt
+          Key='needHelpModalTitle'
+          text='Need Help?'
+          identity='h1'
+        />
+        <Txt
+          Key='needHelpModalSubtitle'
+          text='Please contact us at'
+          isCapitalize={false}
+        />
+        <View>
+          <Icon
+            name='Email'
+            height='20'
+            style={styles.icons}
+          />
+          <Txt
+            Key='needHelpModalEmail'
+            text='support@edgelogistics.com'
+            isCapitalize={false}
+          />
+        </View>
+        <View>
+          <Icon
+            name='Phone'
+            height='20'
+            style={styles.icons}
+          />
+          <Txt
+            Key='needHelpModalPhone'
+            text='312-319-4766'
+            isCapitalize={false}
+          />
+        </View>
+      </View>
     </Modalize>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {},
+  icons: {
+    width: 20,
+    heigth: 20
+  }
 })
 
 NeedHelpModal.propTypes = {
